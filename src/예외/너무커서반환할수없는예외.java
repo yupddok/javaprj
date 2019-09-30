a001d6682454669ee74ca517d41c8d3a6dfc53ca
suyoung <suyoungpk@naver.com>
2019-09-30 15:53:40 +0900

    짝꿍이 주석 달음

--- src/예외/너무커서반환할수없는예외.java
+++ src/예외/너무커서반환할수없는예외.java
@@ -3,6 +3,7 @@ package 예외;
 public class 너무커서반환할수없는예외 extends Exception {
 	@Override
 	public String getMessage() {
+		//화이팅해요- sy ?? 왜 안나오지
 		return "10000이 넘는 값이 반환 되어서는 안되는 오류가 발생하였습니다.";
 	}
 
